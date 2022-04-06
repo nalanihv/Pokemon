946b9c9a5fed2e4b2437c1cedfe08a9aeddd169d
Gabriel-2A <gaa.021205@gmail.com>
2022-04-06 11:28:01 -0500

    Avance 2: Comienzo de la impolementacion de codigo. creacion de getters, setter y constructor en las clases Objeto, habilidad, Pocion y Baya

--- src/Habilidad.java
+++ src/Habilidad.java
@@ -4,4 +4,37 @@ public class Habilidad {
     //descripcion - String
     //ataquebase - int
 
+    private String nombre;
+    private String descripcion;
+    private int ataqueBase;
+
+    public Habilidad(String nombre, String descripcion, int ataqueBase) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.ataqueBase = ataqueBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getAtaqueBase() {
+        return ataqueBase;
+    }
+
+    public void setAtaqueBase(int ataqueBase) {
+        this.ataqueBase = ataqueBase;
+    }
 }
--- src/Pocion.java
+++ src/Pocion.java
@@ -3,8 +3,33 @@ public class Pocion extends Objeto{
     //efecto - string
     //duracion - entero
 
+    private String efecto;
+    private int duracion;
 
-    //usar
+    public Pocion(double costo, int cantidad, String nombre, String tipo, String efecto, int duracion) {
+        super(costo, cantidad, nombre, tipo);
+        this.efecto = efecto;
+        this.duracion = duracion;
+    }
+
+    public String getEfecto() {
+        return efecto;
+    }
+
+    public void setEfecto(String efecto) {
+        this.efecto = efecto;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
+//usar
         //si es curacion
             //aumentar 20 hp
         //si es de fuerza
