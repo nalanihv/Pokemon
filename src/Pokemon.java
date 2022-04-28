50879840565af47f90b1dbad9cb9d5378ba20e2b
Gabriel-2A <gaa.021205@gmail.com>
2022-04-27 22:08:17 -0500

    Avance 3

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -8,6 +8,72 @@ public class Pokemon extends Personaje{
     //fuerte contra - String
     //ataue - int
 
+    private String tipo;
+    private Habilidad habilidad;
+    private int hp;
+    private  boolean esLegendario;
+    private String fuerteContra;
+    private int ataque;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Habilidad getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(Habilidad habilidad) {
+        this.habilidad = habilidad;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isEsLegendario() {
+        return esLegendario;
+    }
+
+    public void setEsLegendario(boolean esLegendario) {
+        this.esLegendario = esLegendario;
+    }
+
+    public String getFuerteContra() {
+        return fuerteContra;
+    }
+
+    public void setFuerteContra(String fuerteContra) {
+        this.fuerteContra = fuerteContra;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public Pokemon(String nombre, int nivel, char genero,
+                   String tipo, Habilidad habilidad, int hp, boolean esLegendario,
+                   String fuerteContra, int ataque) {
+        super(nombre, nivel, genero);
+        this.tipo = tipo;
+        this.habilidad = habilidad;
+        this.hp = hp;
+        this.esLegendario = esLegendario;
+        this.fuerteContra = fuerteContra;
+        this.ataque = ataque;
+    }
 
     //pelea
     // consumi pocion o baya  para ataque?
